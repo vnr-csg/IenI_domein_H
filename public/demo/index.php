057050331999658870afc6887c0203778a567371
vnr-csg <54285347+vnr-csg@users.noreply.github.com>
2024-01-12 15:19:43 +0000

    php => domein H

---
+++ public/demo/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="nl">
+    <head>
+        <meta charset="utf8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="data:,">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+        <title>demo</title>
+    </head>
+    <body class="container">
+        <main class="mx-5 p-5">
+            <div class="container mx-3 my-2 shadow p-5 bg-body rounded">
+                <h1 class="fw-bold text-center mb-4">
+                    <i class="bi bi-tsunami"></i>
+                    Hello World
+                </h1>
+            </div>
+        </main>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
+</html>
